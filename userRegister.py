7a548b6e9a49a44cdf85a55cd665b4ce1c193650
Mehrdad Rahidi <rashidi.mehrdad@gmail.cm>
2023-12-16 15:43:14 +0330

    Third commit

--- userRegister.py
+++ userRegister.py
@@ -2,6 +2,8 @@ import sqlite3
 
 from flask_restful import Resource, reqparse
 
+from user import User
+
 
 class UserRegister(Resource):
     parser = reqparse.RequestParser()
@@ -9,10 +11,14 @@ class UserRegister(Resource):
     parser.add_argument('password', type=str, required=True, help="This field cannot be blank.")
 
     def post(self):
+        data = UserRegister.parser.parse_args()
+
+        if User.find_by_username(data['username']):
+            return {"message": "A user with that username already exists"}, 400
+
         connection = sqlite3.connect('identifier.sqlite')
         cursor = connection.cursor()
 
-        data = UserRegister.parser.parse_args()
         query = "INSERT INTO user VALUES (NULL, ?, ?)"
         cursor.execute(query, (data['username'], data['password']))
         connection.commit()
