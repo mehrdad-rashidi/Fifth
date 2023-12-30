ed5577719f6d67ed36e7170dd62694b08789d6e0
Mehrdad Rahidi <rashidi.mehrdad@gmail.cm>
2023-12-30 14:37:01 +0330

    fifth commit

--- itemList.py
+++ itemList.py
@@ -1,9 +1,16 @@
 from flask_restful import Resource, reqparse
 import sqlite3
 
-items = []
-
 
 class ItemList(Resource):
     def get(self):
+        connection = sqlite3.connect('identifier.sqlite')
+        cursor = connection.cursor()
+        query = "SELECT * FROM items"
+        result = cursor.execute(query)
+        items = []
+        for row in result:
+            items.append({'name': row[0], 'price': row[1]})
+
+        connection.close()
         return {'items': items}
